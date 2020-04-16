c394620426e37e6aae548a807093ebadf2e583a0
Briton Westerhaus <Briton.Westerhaus@gmail.com>
2020-04-15 21:31:53 -0400

    Arrow Controls
    Just a start for that.

---
+++ default.js
@@ -0,0 +1,14 @@
+document.onkeydown = function(evt) {
+    switch(evt.keyCode) {
+        case 37:
+            //left arrow
+            break;
+        
+        case 39:
+            //right arrow
+            break;
+
+        default:
+            break;
+    }
+}

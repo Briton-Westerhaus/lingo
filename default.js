9d9b34b54f97183a27c215c49f0ffc41c8d7693c
Briton Westerhaus <Briton.Westerhaus@gmail.com>
2020-04-18 19:22:41 -0400

    Finished
    The branch is complete.

--- default.js
+++ default.js
@@ -1,14 +1,39 @@
+let selectedInput;
+
+function selectInput(inputNumber) {
+    selectedInput = inputNumber;
+    let input = document.getElementById("input" + inputNumber);
+    input.focus();
+    input.select();
+}
+
+window.onload = function() {
+    selectedInput = 0;
+    this.selectInput(selectedInput);
+}
+
 document.onkeydown = function(evt) {
     switch(evt.keyCode) {
-        case 37:
-            //left arrow
+        case 37: //left arrow
+            if (selectedInput > 0) {
+                selectInput(selectedInput - 1);
+            }
             break;
         
-        case 39:
-            //right arrow
+        case 39: //right arrow
+            if (selectedInput < 5) {
+                selectInput(selectedInput + 1);
+            }
             break;
 
         default:
             break;
     }
+    
+}
+
+document.onkeypress = function(evt) {
+    if (evt.keyCode == 9 && selectedInput < 5) {
+        selectInput(selectedInput + 1);
+    } 
 }

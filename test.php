86356aa367f01d3d087e0e354a4897d7baa97b7a
Briton Westerhaus <Briton.Westerhaus@gmail.com>
2020-04-30 19:54:27 -0400

    Encoded words
    Encoding is done. Test is still there, as well as old words list, in
    case we need to reuse it later.

--- test.php
+++ test.php
@@ -4,7 +4,7 @@
 
     $iv = openssl_random_pseudo_bytes(16);
 
-    fwrite($file2, $iv . "\n");
+    fwrite($file2, $iv);
 
     while(! feof($file)) {
         $word = trim(fgets($file));
@@ -19,15 +19,30 @@
 
     $file = fopen("words.txt","r");
 
-    $iv = trim(fgets($file));
+    $iv = fread($file, 16);
 
-    while(! feof($file)) {
+    $wordCount = (filesize("words.txt") - 16) / 22;
+
+    /*while(! feof($file)) {
         $word = trim(fread($file, 22)) . "==";
         $cipher = "aes-128-cbc";
         //$iv = openssl_random_pseudo_bytes(16);
         $decrypted = openssl_decrypt($word, $cipher, "Briton Westerhaus Lingo", 0, $iv);
         echo $decrypted . "<br />";
-    }
+    }*/
+
+    $rand = rand(0, $wordCount);
+
+    fseek($file, 16 + $rand * 22);
+
+    echo $iv;
+
+    $word = trim(fread($file, 22)) . "==";
+    $cipher = "aes-128-cbc";
+    //$iv = openssl_random_pseudo_bytes(16);
+    $decrypted = openssl_decrypt($word, $cipher, "Briton Westerhaus Lingo", 0, $iv);
+    echo $decrypted . "<br />";
+
 
     fclose($file);
 ?>
